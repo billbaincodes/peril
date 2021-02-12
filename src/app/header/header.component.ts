5aba2ab4b5d911c5ed113fba9e8758e2457e78c3
bill <williamandrewbain@gmail.com>
2021-02-11 18:27:57 -0700

    add back header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { nextContext } from '@angular/core/src/render3';
+import { Observable, of, Subscription } from 'rxjs';
+import { ScoreService } from '../score.service';
 
 @Component({
   selector: 'Header',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // score = new Observable();
+  score: number = 0;
+
+  subscription: Subscription;
+
+  constructor(
+    private scoreService: ScoreService
+  ) { }
+  
 
   ngOnInit() {
+    this.subscription = this.scoreService.getScore().subscribe(score => {
+      if (score) {
+        console.log({ score });
+        this.score = score
+      }
+    });
   }
 
+  getScore(){
+    console.log('this.score', this.score);
+  }
 }
